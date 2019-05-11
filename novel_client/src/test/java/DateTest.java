54a679d894cfe997da48b2fd555ac5446a40375d
lixin <157922018@git.com>
2019-05-11 16:32:09 +0800

    view by lx

---
+++ novel_client/src/test/java/DateTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import javax.sound.midi.Soundbank;
+import java.util.Date;
+
+/**
+ * @author LX
+ * @date 2019/5/11 - 15:26
+ */
+public class DateTest {
+
+    @Test
+    public void dateTest(){
+
+        Date date = new Date(1557560143000l);
+
+        System.out.println(date+"  "+System.currentTimeMillis());
+
+    }
+}
