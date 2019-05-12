363a63414847caa9e050b1ac1d01cb936256a159
lixin <157922018@git.com>
2019-05-12 14:19:31 +0800

    lx

---
+++ novel_backstage/src/main/java/service/impl/IndexNovelServiceImpl.java
@@ -0,0 +1,10 @@
+package service.impl;
+
+import service.IndexNovelService;
+
+/**
+ * @author LX
+ * @date 2019/5/12 - 14:18
+ */
+public class IndexNovelServiceImpl implements IndexNovelService {
+}
