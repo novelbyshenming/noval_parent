c047f5d7a77e4a5828f20490fbab682b7a33060a
lixin <157922018@git.com>
2019-05-13 20:34:32 +0800

    lx

--- novel_client/src/main/java/thrift/datafile/novel2.thrift
+++ novel_client/src/main/java/thrift/datafile/novel2.thrift
@@ -17,19 +17,11 @@ struct NovelChapterContext{
      6: optional String nextChapter;
  }
 
- struct Novel{
-    1: optional String novelName,
-    2: optional String introduction,
-    3: optional String author,
-    4: optional String type,
-    5: optional String image;
-    6: optional list<NovelChapter> novelChaperList;
- }
 
 struct NovelChapter{
 
-   1: optional String novelChapterName,
-   2: optional String novelChapterUrl;
+   1: optional String novelChapterJson,
+
 }
 
 service NovelService{
@@ -37,5 +29,5 @@ service NovelService{
     /*根据 章节的url 来动态爬取 小说内容   用于构建小说页面     */
     NovelChapterContext getNovelChapterContextByChapterUrl(1:required String chapterUrl);
     /*根据  小说的url 来动态爬取 小说章节信息和url    */
-    Novel getNovelChapterListByNovelUrl(1:required String novelUrl);
+    NovelChapter getNovelChapterListByNovelUrl(1:required String novelUrl);
 }
