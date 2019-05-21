a5458618e527984bcec9c189e1b6469d70f4c3d3
ys <814051626@qq.com>
2019-05-21 21:23:23 +0800

    ys

--- upload/src/main/java/controller/UserController.java
+++ upload/src/main/java/controller/UserController.java
@@ -6,11 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.annotations.Param;
 import bean.User;
+import bean.Vip;
+import redis.clients.jedis.Jedis;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yc.util.RedisPoolUtil;
 
 import MyException.UserException;
 import service.UserService;
@@ -25,27 +30,35 @@ public class UserController {
 
 	@RequestMapping(value="login.u",method = RequestMethod.POST)
 	@ResponseBody
-	public HashMap<String, Object> login(User user,HttpServletRequest request) throws UserException, Exception{
-		String msg = "登录成功";
-		HashMap<String,Object> map = new HashMap<String,Object>();//创建一个hashmap存储数据
-		try{
-			if(userService.loginCheck(user) != null){
-			user.setPwd("");//不能讲pwd存入session
-			Long uid = userService.selectUid(user.getName());
-			map.put("msg", msg);//将登录结果信息压入集request.getSession().setAttribute("user", user);//将user存入session合
-			map.put("result", "1");//将登录结果压进map集合
-			map.put("name", user.getName());//将用户名压入集合
-			map.put("uid",uid);//将用户id压入集合
+	public String login(User user,HttpServletRequest request) throws UserException, Exception{
+		String result="";
+		if(userService.loginCheck(user) != null){//登录成功
+			Long uid = userService.selectUid(user.getName());//获得uid
+			Vip vip = userService.selectVip(uid);//得到vip
+			Long nowTimes = System.currentTimeMillis();//获取当前时间
+			Jedis jedis = RedisPoolUtil.getJedis();//redis
+			if(vip != null){
+				if(vip.getFlag() == 1){//判断该用户是否为vip
+					if(nowTimes > vip.getEndTimes()){//判断该用户vip是否过期
+						userService.updateVip(uid);//过期则修改数据库
+						jedis.set("user:"+uid, uid+"");
+					}else{
+						//没有过期
+						jedis.set("vip:"+uid, uid+"");
+						result += "uid="+uid+"&vip=true";
+					}
+				}else{
+					jedis.set("user:"+uid, uid+"");
+					result += "uid="+uid;
+				}
 			}else{
-				throw new UserException("用户名或者密码错误");
+				jedis.set("user:"+uid, uid+"");
+				result += "uid="+uid;
 			}
-			return map;
-		}catch(Exception e ){
-			e.printStackTrace();
-			map.put("msg", e.getMessage());
-			map.put("result","-1");
-			return map;
+		}else{
+			result += "-1";
 		}
+		return result;
 	}
 	
 	
@@ -55,6 +68,7 @@ public class UserController {
 	public HashMap<String, Object> register(@Param("user") User user,HttpServletRequest request,HttpServletResponse response) throws UserException{
 		HashMap<String,Object> map = new HashMap<String,Object>();//创建一个hashmap存储数据
 		String msg = "注册成功";//默认msg为注册成功
+		Vip vip = new Vip();
 		try{
 		//后台继续判断用户名是否为空
 		if(user.getName() == null || user.getName() == ""){
@@ -78,6 +92,15 @@ public class UserController {
 		map.put("uid", user.getUid());//将用户id压入集合
 			//注册成功
 			userService.register(user);
+			Long uid = userService.selectUid(user.getName());
+				Long startTimes = System.currentTimeMillis();
+				Long endTimes = startTimes + 30*24*60*60*1000l;
+				int flag = 1;
+				vip.setStartTimes(startTimes);
+				vip.setEndTimes(endTimes);
+				vip.setFlag(flag);
+				vip.setUid(uid);
+			userService.addVip(vip);//新增vip
 			user.setPwd("");
 			request.getSession().setAttribute("user", user);
 			return map;
